2f9f6e56b1e030b657270a5a81ea3b53dcfc8b3d
Harsh Rana <harsh.rana1@student.kpu.ca>
2026-01-16 14:05:01 -0800

    Add files via upload
    learnt java print command , how to address strings and ints

---
+++ print_string_int_p.java
@@ -0,0 +1,23 @@
+public class print_string_int_p{
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(5+89 + " " + "hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		
+		
+		String name = "harsh";
+		int age = 20;
+		String city = "richmond";
+		String friend = "hiten";
+		String neighbour = friend;
+		
+		
+		System.out.print(neighbour + age + name+ city);
+		
+		
+		
+	}
+}
