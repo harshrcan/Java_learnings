c3ee7dfc38f116b8163cbac3a9d7e01e2d66ef37
Harsh Rana <harsh.rana1@student.kpu.ca>
2026-01-17 01:49:32 -0800

    basic operators

---
+++ operators_p.java
@@ -0,0 +1,45 @@
+public class operators_p{
+	public static void main(String[] args) {
+		
+		//operators
+		int a = 1;
+		int b = 2;
+		int c = 5;
+		int d = 10;
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+		int sum = a+b;
+		System.out.println(sum);
+		
+		int minus = a-b;
+		System.out.println(minus);
+		
+		int mul = a*b;
+		System.out.println(mul);
+		
+		int div = d / c;
+		System.out.println(div);// change variable and div to double to get in decimal points
+		
+		int mod = c % b;
+		System.out.println(mod);//remiander after division
+		
+		
+		// = is the assignment operator
+		///////////////////////////////////////////////////////////////////////////////////////////////////
+		//the following can be placed in front too to get the result first 
+		System.out.println(c);
+		c++;
+		System.out.println(c);
+		a--;
+		System.out.println(a);
+		//the following can be placed in front too to get the result first 
+		
+		System.out.println(c++);
+		System.out.println(c);
+		//and 
+		System.out.println(--d);
+		///////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		
+	}
+}
