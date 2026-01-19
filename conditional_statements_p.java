e9f7a5ce5c6117608fb954e004b9097a5a89df66
Harsh Rana <harsh.rana1@student.kpu.ca>
2026-01-19 07:26:54 -0800

    conditionals - if else

---
+++ conditional_statements_p.java
@@ -0,0 +1,14 @@
+public class conditional_statements_p{
+	public static void main(String[] args) {
+		
+		//conditional statement
+		boolean isSunUp = false;
+		
+		if (isSunUp == true )
+			System.out.println("day");
+		else
+			System.out.println("night");
+		
+		
+	}
+}
