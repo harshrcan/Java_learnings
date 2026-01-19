2fb758dbfac59296fc74a5b38e656a77fc119821
Harsh Rana <harsh.rana1@student.kpu.ca>
2026-01-19 07:24:14 -0800

    added more operators

--- operators_p.java
+++ operators_p.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class operators_p{
 	public static void main(String[] args) {
 		
@@ -38,6 +40,56 @@ public class operators_p{
 		//and 
 		System.out.println(--d);
 		///////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//comparison operator
+		//a==b are the values equal
+		//not equal to //a != b 
+		//a < b
+		//a > b
+		// a <= b
+		// a >= b
+		
+		///////////////////////////////////////////////////////////////////////////////////////////////////
+		/// logical operators
+		/// && and
+		/// || or
+		/// ! not
+		/// 
+		int x = 300;
+		int y = 840;
+		
+		if (x <50 && y < 50)
+			System.out.println("both are less then 50");
+		else if (x < 50 || y < 50)
+			System.out.println("one is not less then 50");
+		else 
+			System.out.println("both are not less then 50");
+		
+		boolean isAdult = false;
+		if ( ! isAdult)
+			System.out.println("not an adult");
+		else
+			System.out.println("is adult");
+		
+		////////////////////////////////////////////////////////////////////
+		Scanner sc = new Scanner(System.in);
+		//pen = 10 ; notebook = 40
+		System.out.println("Enter the amount u have :");
+		int cash = sc.nextInt();
+		if (cash < 10)
+		{System.out.println("cannot buy anything");
+		System.out.println("get more cash");
+		}
+		else if (cash > 10 && cash <50) {
+			System.out.println("can get one item");
+				
+		}
+				else {
+					
+					System.out.println("can get both");
+				}
+			
+		
 		
 		
 		
