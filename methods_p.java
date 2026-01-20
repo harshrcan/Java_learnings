107740278def207022d2c092d990f1236164544c
Harsh Rana <harsh.rana1@student.kpu.ca>
2026-01-20 02:38:47 -0800

    functions/methods in java

---
+++ methods_p.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class methods_p{
+	
+public static void main(String[] args) {
+	
+		///functions - methods
+		 printName("harsh rana ji ");
+		printHarsh();
+		printHarsh();
+		printHarsh();
+		printHarsh();
+		}
+
+////////////////////////////////////////////
+	public static void printHarsh() {
+		System.out.println("Harsh RAna");
+	}
+	
+	//////////////////////////////////////////////////////
+	
+	public static void printName(String name) {
+		System.out.println(name);
+	}
+	
+	
+	
+	
+	}
